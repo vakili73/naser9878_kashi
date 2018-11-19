ee6f968b62506827a548c1c85e60c52ffbc05664
Vahid Vakili <v.vakili73@gmail.com>
2018-11-19 14:54:38 +0330

    First Commit...

---
+++ naser-9878-kashi/test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {Naser9878KashiApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new Naser9878KashiApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: Naser9878KashiApplication;
+  client: Client;
+}
